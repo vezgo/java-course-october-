4be026961eb2f7e27de70e18d220599c07de78cc
vezgo <vezgo1903@gmail.com>
2020-11-08 17:13:30 +0200

    initial commit 2

---
+++ QAhomeworknovember/src/main/java/homework.java
@@ -0,0 +1,20 @@
+public class homework {
+
+    String string01 ="This is a test!";
+    /
+}
+
+    public static void main(String[]args) {
+        homework homework = new homework();
+        //1
+        homework.replaceString();
+        //2
+        System.out.println(homework.areStringsEqual(homework.string01."This is a test!"));
+        //3
+        System.out.println("Number of string words is" + homework.getStringPartscount());
+        //4
+        homework.concatStrings("String1", "String2");
+        //5
+        homework.getStringchar(charindex);
+    }
+
