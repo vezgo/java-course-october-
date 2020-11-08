d4926ca67f82c11bfff5b907eb33328eeb20eacf
vezgo <vezgo1903@gmail.com>
2020-11-08 09:52:35 +0200

    initial commit

---
+++ src/main/java/com/lectures01/Main.java
@@ -0,0 +1,14 @@
+package com.lectures01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanObject=new Scanner(System.in);
+                String name=scanObject.nextLine();
+                            String favourite  = scanObject.nextLine();
+System.out.println("Name: "+name);
+    System.out.println("Favourite: "+favourite);
+    }
+}
+
